768bdbe50d223284e1b38436870abdaf7d838a39
StanLukuvka <stanluku@gmail.com>
2026-09-19 22:53:34 +1000

    refactor: delete legacy Automatic compatibility module

--- speed_scripts/automatic_config.py
+++
@@ -1,8 +0,0 @@
-"""Compatibility re-exports for the pre-consolidation Automatic planner module."""
-
-from .planning import STAGES_TO_SCALES, build_automatic_speed_config
-
-__all__ = [
-    "STAGES_TO_SCALES",
-    "build_automatic_speed_config",
-]
